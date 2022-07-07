6cb791ef6ade24259db505537f3971046832c0c2
Shoaib Shaikh <shaikhsho292@gmail.com>
2022-07-07 17:24:21 +0530

    img-change

--- src/components/layouts/RoadMap.jsx
+++ src/components/layouts/RoadMap.jsx
@@ -24,6 +24,11 @@ const RoadMap = (props) => {
                 className='sub-title mb-10'
                 data-aos='fade-up'
                 data-aos-duration='1000'
+                style={{
+                  textShadow:
+                    "0 0 9px rgb(255 255 255 / 10%), 0 0 10px rgb(255 255 255 / 30%), 0 0 21px rgb(255 255 255 / 20%), 0 0 82px rgb(255 255 255 / 10%)",
+                  fontSize: "27px",
+                }}
               >
                 Road Map? NO!
               </h5>
@@ -58,7 +63,18 @@ const RoadMap = (props) => {
                   >
                     {data.title}
                   </h5>
-                  <p className='fs-18'>{data.desc} </p>
+                  {data.customDesc ? (
+                    <p>
+                      5 or more NFTs you get to talk with Uorfi on a call.
+                      <br />
+                      10 or more NFTs you get to talk with Uorfi on a video
+                      call.
+                      <br />
+                      20 or more NFTs you get to meet Uorfi in person.
+                    </p>
+                  ) : (
+                    <p className='fs-18'>{data.desc} </p>
+                  )}
                 </div>
               ))}
             </div>
